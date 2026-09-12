0aef1a4963933011dedd8a11fe19474feb19ccd8
PATEL NEEV ASHIT <neevpatel2600@gmail.com>
2026-09-12 22:49:26 +0530

    Add Easy Problem [A] from Codeforces

---
+++ 2044/A - Easy Problem/solution.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int count=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                if(i+j==n){
+                    count++;
+                }
+            }
+        }
+        cout<<count<<endl;
+    }
+}
