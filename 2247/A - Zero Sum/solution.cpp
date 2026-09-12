b5bfba121b3e1492ccce0c24fa83c5ab3d73e065
PATEL NEEV ASHIT <neevpatel2600@gmail.com>
2026-09-12 22:53:08 +0530

    Add Zero Sum [A] from Codeforces

---
+++ 2247/A - Zero Sum/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            sum += x;
+        }
+ 
+        if (n % 2 == 0 && sum % 4 == 0)
+            cout << "YES"<<endl;
+        else
+            cout << "NO"<<endl;
+    }
+ 
+    return 0;
+}
