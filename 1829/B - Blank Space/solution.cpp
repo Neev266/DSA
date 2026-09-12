f20a74063828c59d3ed81e7ed6b585779b04ae27
PATEL NEEV ASHIT <neevpatel2600@gmail.com>
2026-09-12 22:52:15 +0530

    Add Blank Space [B] from Codeforces

---
+++ 1829/B - Blank Space/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++){
+	        cin>>arr[i];
+	    }
+	    int cnt=0;
+	    int maxi=INT_MIN;
+	    for(int i=0;i<n;i++){
+	        if(arr[i]==0){
+	            cnt++;
+	        }else{
+	            maxi=max(maxi,cnt);
+	            cnt=0;
+	        }
+	    }
+	    maxi=max(maxi,cnt);
+	    cout<<maxi<<endl;
+	}
+ 
+}
