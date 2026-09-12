5baf17670e195b6b8603ca65c2a727a9686db693
PATEL NEEV ASHIT <neevpatel2600@gmail.com>
2026-09-12 22:51:57 +0530

    Add Iskander and Drawings [A] from Codeforces

---
+++ 2244/A - Iskander and Drawings/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    string s;
+	    cin>>s;
+	    int maxi=INT_MIN;
+	    int cnt=0;
+	    for(int i=0;i<n;i++){
+	        if(s[i]=='#'){
+	            cnt++;
+	        }else{
+	            maxi=max(maxi,cnt);
+	            cnt=0;
+	        }
+	    }
+	    maxi=max(maxi,cnt);
+	    cout<<ceil((double)maxi/2)<<endl;
+	}
+ 
+}
