c9aec73cbe3cc756ce4cb2940090018022d73445
PATEL NEEV ASHIT <neevpatel2600@gmail.com>
2026-09-12 22:50:12 +0530

    Add Isamatdin and His Magic Wand! [C] from Codeforces

---
+++ 2167/C - Isamatdin and His Magic Wand!/solution.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        bool hasEven = false, hasOdd = false;
+ 
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            if (a[i] % 2 == 0) hasEven = true;
+            else hasOdd = true;
+        }
+ 
+        if (hasEven && hasOdd) {
+            sort(a.begin(), a.end());
+        }
+ 
+        for (int i = 0; i < n; i++) {
+            cout << a[i] << " ";
+        }
+        cout << "
+";
+    }
+ 
+    return 0;
+}
